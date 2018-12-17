76b14839db72be870ee70530a5fef875ac9cc974
Ed Courtney <edcourtney74@gmail.com>
2018-12-17 08:26:39 -0500

    Added more friends to friends array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,56 @@
 // Contains an array of every person who has filled out the survey and their responses
-var friendsData = [];
+var friendsData = [
+    {
+        name: "Tom Haverford",
+        image: "https://d1o51r9qdgnnlz.cloudfront.net/items/28fdbb09-1083-4da6-ace4-12384e2a3299.png",
+        scores: [1, 1, 5, 1, 3, 4, 1, 5, 5, 2]
+    },
+    {
+        name: "Andy Dwyer",
+        image: "https://s3.amazonaws.com/dailybreak_images_prod/991fb5ed-a1b5-4e9d-84c2-956b691d1460",
+        scores: [1, 3, 2, 2, 1, 5, 3, 5, 4, 1]
+    },
+    {
+        name: "Ron Swanson",
+        image: "https://uproxx.files.wordpress.com/2016/06/ron-swanson-feature.jpg?quality=95&w=650&h=360",
+        scores: [3, 4, 1, 1, 4, 1, 5, 1, 1, 2]
+    },
+    {
+        name: "Leslie Knope",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png/240px-Leslie_Knope_%28played_by_Amy_Poehler%29.png",
+        scores: [1, 3, 2, 2, 1, 5, 3, 5, 4, 1]
+    },
+    {
+        name: "April Ludgate",
+        image: "https://thoughtcatalog.files.wordpress.com/2016/04/screen-shot-2016-04-27-at-1-32-41-pm.png?w=743&h=415",
+        scores: [2, 2, 2, 1, 2, 5, 3, 2, 1, 2]
+    },
+    {
+        name: "Donna Meagle",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Donna_Meagle.jpg/250px-Donna_Meagle.jpg",
+        scores: [2, 3, 5, 2, 3, 4, 1, 4, 3, 2]
+    },
+    {
+        name: "Ben Wyatt",
+        image: "https://imagesvc.timeincapp.com/v3/fan/image?url=https://culturess.com/wp-content/blogs.dir/386/files/2017/03/635928161632591870-426328109_ddddddd.jpg&c=sc&w=1916&h=1078",
+        scores: [5, 4, 2, 2, 5, 3, 5, 1, 1, 2]
+    },
+    {
+        name: "Jerry Gergich",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Jerry_Gergich.jpg/220px-Jerry_Gergich.jpg",
+        scores: [2, 5, 2, 3, 4, 1, 5, 4, 3, 2]
+    },
+    {
+        name: "Chris Traeger",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Chris_Traeger.jpg/218px-Chris_Traeger.jpg",
+        scores: [4, 4, 3, 2, 5, 1, 4, 3, 5, 5]
+    },
+    {
+        name: "Ann Perkins",
+        image: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/33/Ann_Perkins.jpg/revision/latest?cb=20180116070354",
+        scores: [3, 4, 3, 2, 4, 1, 4, 4, 3, 5]
+    }
+];
 
 // Exports friendsData for use in other files
 module.exports = friendsData;
